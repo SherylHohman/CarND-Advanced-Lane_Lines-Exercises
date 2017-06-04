eb68a85b824fd6e7fc6e59658efaa48ff3ac266d
SherylHohman <SherylHohman@users.noreply.github.com>
2017-06-03 19:39:59 -0700

    finish mag_thresh function

--- L_22_magnitude_of_gradient.py
+++ L_22_magnitude_of_gradient.py
@@ -1,15 +1,17 @@
 
 # coding: utf-8
 
-# ### L_22 Magnitude of the Gradient
-# #### my solution
+# ### L_22 Magnitude of the Gradient  
+# ##### My Output for this function, when run online, looks like this:   
+# <img src='l22-my-output-of-thresh-mag.png' />  
+# #### my solution:  
 
-# In[ ]:
+# In[1]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[ ]:
+# In[2]:
 
 import numpy as np
 import cv2
@@ -28,24 +30,48 @@ def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
     
     # Apply the following steps to img
     # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
     # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    
     # 3) Calculate the magnitude 
+    abs_sobel = np.sqrt(np.square(sobelx) + np.square(sobely))
+
     # 4) Scale to 8-bit (0 - 255) and convert to type = np.uint8
+    scaled = np.uint8(255 * abs_sobel/np.max(abs_sobel))
+    
     # 5) Create a binary mask where mag thresholds are met
+    binary_output = np.zeros_like(gray)
+    binary_output[ ((scaled >= mag_thresh[0]) & (scaled <= mag_thresh[1])) ] = 1
+    
     # 6) Return this mask as your binary_output image
-    binary_output = np.copy(img) # Remove this line
+    #binary_output = np.copy(img) # Remove this line
     return binary_output
     
 # Run the function
 mag_binary = mag_thresh(image, sobel_kernel=3, mag_thresh=(30, 100))
+
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
+
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
+
 ax2.imshow(mag_binary, cmap='gray')
 ax2.set_title('Thresholded Magnitude', fontsize=50)
+
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+
+
+# ### My Output for this function, when run online, looks like this:  
+# <img src='l22-my-output-of-thresh-mag.png' />
+
+# In[ ]:
+
+
 
 
 # In[ ]:
