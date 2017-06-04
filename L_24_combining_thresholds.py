f41e277c6ea6fc58786b961d98b70c12f91ed0eb
SherylHohman <SherylHohman@users.noreply.github.com>
2017-06-04 04:20:16 -0700

    add L24 Combining Thresholds start code, and instructions

---
+++ L_24_combining_thresholds.py
@@ -0,0 +1,106 @@
+
+# coding: utf-8
+
+# ### Combining Thresholds
+
+# In[ ]:
+
+get_ipython().magic('matplotlib inline')
+
+
+# In[ ]:
+
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    # Calculate directional gradient
+    # Apply threshold
+    return grad_binary
+
+def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Calculate gradient magnitude
+    # Apply threshold
+    return mag_binary
+
+def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+    # Calculate gradient direction
+    # Apply threshold
+    return dir_binary
+
+# Choose a Sobel kernel size
+ksize = 3 # Choose a larger odd number to smooth gradient measurements
+
+# Apply each of the thresholding functions
+gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
+grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
+dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+
+combined = np.zeros_like(dir_binary)
+combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+
+
+
+# In[ ]:
+
+
+
+
+# #### Read Me
+# #### instructions
+
+# ### Combining Thresholds  
+# If you play around with the thresholds in the last exercise, you'll find that you can start to identify the lane lines by gradient direction alone by setting the threshold around `thresh = (0.7, 1.3`), but there's still a lot of noise in the resulting image.  
+# 
+# Now consider how you can use various aspects of your gradient measurements ***(x, y, magnitude, direction)** to isolate lane-line pixels. Specifically, think about how you can use thresholds of the x and y gradients, the overall gradient magnitude, and the gradient direction to focus on **pixels that are likely to be part of the lane lines**.  
+# 
+# #### Challenge:  
+# In the project at the end of this section, you'll want to experiment with thresholding various aspects of the gradient, so now would be a great time to start coding it up on your local machine! Grab the image we've been working with for the last three quizzes *here (or a smaller jpg file here)*.  
+# 
+# Combine the selection thresholds from the last 3 quizzes to write a piece of code like the following, where you can play with various thresholds and see the output.  
+# 
+# ```
+# def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+#     # Calculate directional gradient
+#     # Apply threshold
+#     return grad_binary
+# 
+# def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+#     # Calculate gradient magnitude
+#     # Apply threshold
+#     return mag_binary
+# 
+# def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+#     # Calculate gradient direction
+#     # Apply threshold
+#     return dir_binary
+# 
+# # Choose a Sobel kernel size
+# ksize = 3 # Choose a larger odd number to smooth gradient measurements
+# 
+# # Apply each of the thresholding functions
+# gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
+# grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
+# mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
+# dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+# ```
+# Try different combinations and see what you get.  
+# 
+# For example, here is a selection for pixels where either: 
+#   - both the x and y gradients meet the threshold criteria, or 
+#   - both the gradient magnitude and direction are within their threshold values.  
+# 
+# ```
+# combined = np.zeros_like(dir_binary)
+# combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+# ```
+# Output  
+# Here is an example of a binary result from multiple thresholds:  
+# <img src='l24-binary-combo-example.jpg' />
+
+# In[ ]:
+
+
+
