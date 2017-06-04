58f44db523ea973ca3bb49d3ef45f4cb3d4f69a9
SherylHohman <SherylHohman@users.noreply.github.com>
2017-06-04 08:42:39 -0700

    Finish "Combining Thresholds". TODO:find good vals
    functions are written.
    Now need to tune threshod vaues and ksize
    to obtain result image with good lane lines

--- L_24_combining_thresholds.py
+++ L_24_combining_thresholds.py
@@ -3,44 +3,85 @@
 
 # ### Combining Thresholds
 
-# In[ ]:
+# In[9]:
 
 get_ipython().magic('matplotlib inline')
 
 
-# In[ ]:
+# In[17]:
 
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
-def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+def abs_sobel_thresh(gray, orient='x', sobel_kernel=3, thresh=(0, 255)):
     # Calculate directional gradient
+    dirx, diry = (0,0)
+    if   orient == 'x': dirx = 1
+    elif orient == 'y': diry = 1
+    else: print('invalid orientation must be "x" or "y"')
+    sobel = cv2.Sobel(gray, cv2.CV_64F, dirx, diry, ksize=sobel_kernel)
+    abs_sobel = np.absolute(sobel)
+    scaled = np.uint8(255 * abs_sobel/np.max(abs_sobel))
     # Apply threshold
+    grad_binary = np.zeros_like(gray)
+    grad_binary[(sobel >= thresh[0]) & (sobel <= thresh[1]) ] = 1
     return grad_binary
 
-def mag_thresh(image, sobel_kernel=3, mag_thresh=(0, 255)):
+def mag_thresh(gray, sobel_kernel=3, mag_thresh=(0, 255)):
     # Calculate gradient magnitude
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    abs_sobel = np.sqrt(np.square(sobelx) + np.square(sobely))
+    scaled = np.uint8(255*abs_sobel/np.max(abs_sobel))
     # Apply threshold
+    mag_binary = np.zeros_like(gray)
+    mag_binary[ (scaled >= mag_thresh[0]) & scaled <= mag_thresh[1] ] = 1
     return mag_binary
 
-def dir_threshold(image, sobel_kernel=3, thresh=(0, np.pi/2)):
+def dir_threshold(gray, sobel_kernel=3, thresh=(0, np.pi/2)):
     # Calculate gradient direction
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobelx_abs = np.absolute(sobelx)
+    sobely_abs = np.absolute(sobely)
+    dir = np.arctan2(sobely_abs, sobelx_abs)
     # Apply threshold
+    dir_binary = np.zeros_like(gray)
+    dir_binary[ (dir <= thresh[0]) & (dir >= thresh[1]) ] = 1
     return dir_binary
+
+image_color = mpimg.imread('signs_vehicles_xygrad.png')
+# convert to gray here so its only done once. Don'e need the color version inside other functions
+image_gray  = cv2.cvtColor(image_color, cv2.COLOR_RGB2GRAY)
+
 
 # Choose a Sobel kernel size
 ksize = 3 # Choose a larger odd number to smooth gradient measurements
 
+
 # Apply each of the thresholding functions
-gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(0, 255))
-grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(0, 255))
-mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(0, 255))
-dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0, np.pi/2))
+gradx = abs_sobel_thresh(  image_gray, orient='x', sobel_kernel=ksize, thresh=    (0, 255)) #(0,255)
+grady = abs_sobel_thresh(  image_gray, orient='y', sobel_kernel=ksize, thresh=    (0, 255)) #(0,255)
+mag_binary = mag_thresh(   image_gray,             sobel_kernel=ksize, mag_thresh=(5, 200)) #(0,255)
+dir_binary = dir_threshold(image_gray,             sobel_kernel=ksize, thresh=(0.7, 1.3)) #(0, np.pi/2)
 
 combined = np.zeros_like(dir_binary)
 combined[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
 
+
+# Plot the result
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+f.tight_layout()
+
+ax1.imshow(image_color)
+ax1.set_title('Original Image', fontsize=50)
+
+ax2.imshow(combined, cmap='gray')
+ax2.set_title('Combined Thresholds', fontsize=50)
+
+plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 
 
 # In[ ]:
