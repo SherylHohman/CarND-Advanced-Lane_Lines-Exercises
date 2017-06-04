e05517048440960513b87eb3ead8a2bc223cebf6
SherylHohman <SherylHohman@users.noreply.github.com>
2017-06-03 20:55:30 -0700

    WIP dir_threshold function

--- L_23_direction_of_gradient.py
+++ L_23_direction_of_gradient.py
@@ -29,23 +29,39 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     
     # Apply the following steps to img
     # 1) Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    
     # 2) Take the gradient in x and y separately
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernal)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernal)
+    
     # 3) Take the absolute value of the x and y gradients
-    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient 
+    abs = np.sqrt(np.square(sobelx)+np.square(sobely))
+    
+    # 4) Use np.arctan2(abs_sobely, abs_sobelx) to calculate the direction of the gradient
+    
+    
     # 5) Create a binary mask where direction thresholds are met
+    binary_output = np.zeros_like(gray)
+    binary_output[ ((scaled>=thresh[0]) & (scaled<=thresh[1])) ] = 1
+    
     # 6) Return this mask as your binary_output image
     binary_output = np.copy(img) # Remove this line
     return binary_output
     
 # Run the function
 dir_binary = dir_threshold(image, sobel_kernel=15, thresh=(0.7, 1.3))
+
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
+
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
+
 ax2.imshow(dir_binary, cmap='gray')
 ax2.set_title('Thresholded Grad. Dir.', fontsize=50)
+
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 
 
