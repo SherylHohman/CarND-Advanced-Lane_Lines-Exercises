f52f8903d2424f8b2e3d70dbad339cb20ed2a9a6
SherylHohman <SherylHohman@users.noreply.github.com>
2017-06-05 14:42:58 -0700

    add source image; point out possible bug in start code

--- L_30_color_and_gradient.py
+++ L_30_color_and_gradient.py
@@ -17,16 +17,33 @@ import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-
 image = mpimg.imread('bridge_shadow.jpg')
 
+
 # Edit this function to create your own pipeline.
-def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
-    img = np.copy(img)
+ 
+def pipeline(orig_image, s_thresh=(170, 255), sx_thresh=(20, 100)):
+# def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+
+    #     ## !! LINE BELOW :: HOW DOES DEEP COPYING TO ITSELF HELP ??!!
+    #     ## WHAT AM I MISSING HERE ?
+    #     img = np.copy(img)
+    #
+    #     # SHOULD IT NOT BE: ??
+    #     img_copy = np.copy(img) 
+    #    
+    #  -SH
+    
+    # SH:  changed name of passed in image to "orig_image" in function definition.  
+    # Now I can make a copy:
+    img = np.copy(orig_image)
+    
+    
     # Convert to HSV color space and separate the V channel
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
     l_channel = hsv[:,:,1]
     s_channel = hsv[:,:,2]
+    
     # Sobel x
     sobelx = cv2.Sobel(l_channel, cv2.CV_64F, 1, 0) # Take the derivative in x
     abs_sobelx = np.absolute(sobelx) # Absolute x derivative to accentuate lines away from horizontal
@@ -39,10 +56,12 @@ def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     # Threshold color channel
     s_binary = np.zeros_like(s_channel)
     s_binary[(s_channel >= s_thresh[0]) & (s_channel <= s_thresh[1])] = 1
+    
     # Stack each channel
-    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. It might
-    # be beneficial to replace this channel with something else.
+    # Note color_binary[:, :, 0] is all 0s, effectively an all black image. 
+    # It might be beneficial to replace this channel with something else.
     color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary))
+    
     return color_binary
     
 result = pipeline(image)
@@ -237,9 +256,21 @@ import matplotlib.image as mpimg
 
 image = mpimg.imread('bridge_shadow.jpg')
 
+
 # Edit this function to create your own pipeline.
 def pipeline(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+
+    #     ## !! LINE BELOW :: HOW DOES DEEP COPYING TO ITSELF HELP ??!!
+    #     ## WHAT AM I MISSING HERE ?
+    #     img = np.copy(img)
+    #
+    #     # SHOULD IT NOT BE: ??
+    #     img_copy = np.copy(img) 
+    #    
+    # -SH
+
     img = np.copy(img)
+    
     # Convert to HSV color space and separate the V channel
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HLS).astype(np.float)
     l_channel = hsv[:,:,1]
