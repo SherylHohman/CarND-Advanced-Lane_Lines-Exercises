5c4d50014a875baadeae11fd449b378da781f25b
SherylHohman <SherylHohman@users.noreply.github.com>
2017-06-04 23:52:08 -0700

    write hls_select(). TODO determine thresh values

--- L_29_hls_color_threshold.py
+++ L_29_hls_color_threshold.py
@@ -5,7 +5,12 @@
 # 
 # #### My Solution
 
-# In[ ]:
+# In[6]:
+
+get_ipython().magic('matplotlib inline')
+
+
+# In[7]:
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -17,11 +22,17 @@ image = mpimg.imread('test6.jpg')
 
 # Define a function that thresholds the S-channel of HLS
 # Use exclusive lower bound (>) and inclusive upper (<=)
+
 def hls_select(img, thresh=(0, 255)):
     # 1) Convert to HLS color space
+    hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    S = hls[:,:,2]
+    
     # 2) Apply a threshold to the S channel
+    binary_output = np.zeros_like(img)
+    binary_output[ (S > thresh[0]) & (S <= thresh[1]) ] = 1
+    
     # 3) Return a binary image of threshold result
-    binary_output = np.copy(img) # placeholder line
     return binary_output
     
 hls_binary = hls_select(image, thresh=(0, 255))
@@ -29,10 +40,13 @@ hls_binary = hls_select(image, thresh=(0, 255))
 # Plot the result
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
 f.tight_layout()
+
 ax1.imshow(image)
 ax1.set_title('Original Image', fontsize=50)
+
 ax2.imshow(hls_binary, cmap='gray')
 ax2.set_title('Thresholded S', fontsize=50)
+
 plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
 
 
